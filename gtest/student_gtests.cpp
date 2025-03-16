bd724c791d8534a13aaa6022937c540f9400c5df
Arianna Gonzalez <adrianyg@uci.edu>
2025-03-16 04:27:52 +0000

    I am checking to see if the dijkstras is correct

--- gtest/student_gtests.cpp
+++ gtest/student_gtests.cpp
@@ -25,3 +25,47 @@ TEST(Dijkstra, small){
   vector<int> expected_path = {0, 3, 1, 2};
   EXPECT_EQ(path, expected_path);
 }
+
+TEST(DijkstraTest, SmallGraph) {
+    Graph new_graph;
+
+    // Load the graph from file
+    std::ifstream infile("../src/medium.txt");  // Ensmere this path is correct
+    infile >> new_graph;
+    infile.close();
+
+    // Run Dijkstra’s algorithm from node 0
+    vector<int> previous(new_graph.numVertices, -1);
+    vector<int> distances = dijkstra_shortest_path(new_graph, 0, previous);
+
+    // Expected shortest distances
+    vector<int> expected_distances = {0, 5, 3, 12, 10, 2};
+
+    // Expected paths
+    vector<int> expected_path_to_2 = {0, 5, 2};  // Shortest path to node 2
+    vector<int> expected_path_to_3 = {0, 5, 2, 3};  // Shortest path to node 3
+    vector<int> expected_path_to_4 = {0, 5, 4};  // Shortest path to node 4
+
+    // Assertions
+    EXPECT_EQ(distances, expected_distances);
+
+    EXPECT_EQ(extract_shortest_path(distances, previous, 2), expected_path_to_2);
+    EXPECT_EQ(extract_shortest_path(distances, previous, 3), expected_path_to_3);
+    EXPECT_EQ(extract_shortest_path(distances, previous, 4), expected_path_to_4);
+}
+
+TEST(Dijkstra, print){
+  Graph G;
+  std::ifstream infile("../src/medium.txt");
+  infile >> G;
+  infile.close();
+  int destination = 3;
+  vector<int> previous;
+  vector<int> distances = dijkstra_shortest_path(G, 0, previous);
+  vector<int> path = extract_shortest_path(distances, previous, destination);
+  int total_cost = (distances[destination] == INF) ? -1 : distances[destination];
+  testing::internal::CaptureStdout();
+  print_path(path, total_cost);
+  std::string output = testing::internal::GetCapturedStdout();
+  EXPECT_EQ(output, "0 5 2 3\nTotal cost: 12\n");
+}
--- src/dijkstras.cpp
+++ src/dijkstras.cpp
@@ -49,7 +49,7 @@ void print_path(const vector<int>& v, int total) {
     }
 
     for (size_t i = 0; i < v.size(); ++i) {
-        if (i > 0) cout << " -> ";
+        if (i > 0) cout << " ";
         cout << v[i];
     }
     
